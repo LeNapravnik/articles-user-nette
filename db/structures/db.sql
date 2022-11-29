30a9c588cd0e95e9617f021ad44268d426c59063
LeNapravnik <lezinapoli@gmail.com>
2022-11-29 06:59:11 +0100

    Changed datetime to timestamp, added foreign key into rating table

--- db/structures/db.sql
+++ db/structures/db.sql
@@ -7,28 +7,36 @@ CREATE TABLE IF NOT EXISTS `article` (
     `title` VARCHAR(255) NULL DEFAULT NULL , 
     `description` VARCHAR(255) NULL DEFAULT NULL , 
     `content` TEXT NOT NULL , 
-    `insertion_date` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP , 
+    `insertion_date` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP , 
     `rating` INT NULL DEFAULT '0', 
     `visibility` ENUM('all_users','logged_users') NOT NULL DEFAULT 'all_users' , 
     PRIMARY KEY (`id`) ,
-    UNIQUE `link` (`link`) USING BTREE) ENGINE = InnoDB;
+    UNIQUE `link` (`link`) USING BTREE
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
     
 CREATE TABLE IF NOT EXISTS `user` (
-    `id` INT(11) NOT NULL AUTO_INCREMENT , 
+    `id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT , 
     `username` VARCHAR(255) NOT NULL , 
     `mail` VARCHAR(100) NOT NULL ,
     `passwd` VARCHAR(60) NOT NULL , 
     `role` ENUM('admin','member') NOT NULL DEFAULT 'member', 
-    `registration_date` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP , 
+    `registration_date` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP , 
     `active` BOOLEAN NOT NULL DEFAULT TRUE , 
     PRIMARY KEY (`id`) , 
-    UNIQUE `username` (`username`) USING BTREE) ENGINE = InnoDB;
+    UNIQUE `username` (`username`) USING BTREE
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
     
 CREATE TABLE IF NOT EXISTS `rating` (
     `id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT, 
     `article_id` INT(11) UNSIGNED NOT NULL , 
     `user_id` INT(11) UNSIGNED NOT NULL , 
     `value` INT NOT NULL , 
-    `time` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ,
+    `time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ,
     PRIMARY KEY (`id`) ,
-    UNIQUE KEY `article_user` (`article_id`,`user_id`) USING BTREE) ENGINE = InnoDB;
+    KEY `article_id` (`article_id`),
+    CONSTRAINT `article_id` 
+        FOREIGN KEY (`article_id`) 
+        REFERENCES `article` (`id`) 
+        ON DELETE CASCADE ON UPDATE CASCADE,
+    UNIQUE KEY `article_user` (`article_id`,`user_id`) USING BTREE
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
